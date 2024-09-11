327694243a5a7b79c56b78a5ff754721cbf9d30e
AmitVedpathak <amitvedpathak2003@gmail.com>
2024-09-11 13:23:30 +0530

    small changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import { Inputbox } from "./components/index";
 
 function App() {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState();
   const [from, setForm] = useState("usd");
   const [to, setTo] = useState("inr");
   const [convertedAmount, setConvertedAmount] = useState(0);
